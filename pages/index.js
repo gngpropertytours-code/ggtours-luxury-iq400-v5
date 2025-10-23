a07a7a64275c57995207a156c95e963d2b4fad22
gngpropertytours-code <gng.propertytours@gmail.com>
2025-10-23 05:39:36 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,25 @@
 import Hero from '../components/Hero';
 import HowItWorks from '../components/HowItWorks';
-import Plans from '../pages/plans'; // we’re importing the section here
+import Plans from '../pages/plans';
+import FadeInSection from '../components/FadeInSection';
 
 export default function Home() {
   return (
     <>
-      <Hero />
-      <HowItWorks />
-      <Plans />
+      {/* Hero Section */}
+      <FadeInSection>
+        <Hero />
+      </FadeInSection>
+
+      {/* How It Works Section */}
+      <FadeInSection>
+        <HowItWorks />
+      </FadeInSection>
+
+      {/* Plans Section */}
+      <FadeInSection>
+        <Plans />
+      </FadeInSection>
     </>
   );
 }
