0e2b25261540aacfdc08fbd723dccd18a7993e1b
gngpropertytours-code <gng.propertytours@gmail.com>
2025-10-23 07:28:00 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,14 @@
-import Hero from "../components/Hero";
-import HowItWorks from "../components/HowItWorks";
-import Plans from "../components/Plans";
+import FadeInSection from '../components/FadeInSection';
+import Hero from '../components/Hero';
+import HowItWorks from '../components/HowItWorks';
+import Plans from '../components/Plans';
 
 export default function Home() {
   return (
-    <div
-      style={{
-        backgroundColor: "black",
-        color: "white",
-        fontFamily: "Inter, sans-serif",
-      }}
-    >
-      <Hero />
-      <HowItWorks />
-      <Plans />
-    </div>
+    <>
+      <FadeInSection><Hero /></FadeInSection>
+      <FadeInSection><HowItWorks /></FadeInSection>
+      <FadeInSection><Plans /></FadeInSection>
+    </>
   );
 }
