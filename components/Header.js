460a81789a0aca41c0d4fe1eb02f08851ae634d6
gngpropertytours-code <gng.propertytours@gmail.com>
2025-10-23 04:59:35 -0400

    Add Header component with styled header element

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <header style={{
+      borderBottom: '1px solid',
+      borderImage: 'linear-gradient(90deg,#C8A951,#D4AF37,#EAD27F) 1',
+      padding: '1rem'
+    }}>
+      <h2 style={{ color: '#fff' }}>G & G Property Tours</h2>
+    </header>
+  );
+}
