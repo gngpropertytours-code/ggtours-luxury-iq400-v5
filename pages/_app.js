3d6923e43f526a9fb5046e436952dfbf03603d3d
gngpropertytours-code <gng.propertytours@gmail.com>
2025-10-23 10:38:36 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,24 @@
-import '../styles/global.css';
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
+import { useEffect } from "react";
+import "../styles/global.css";
 
-export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <Navbar />
-      <Component {...pageProps} />
-    </>
-  );
+function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    const progressBar = document.createElement("div");
+    progressBar.id = "scroll-progress";
+    document.body.appendChild(progressBar);
+
+    const updateProgress = () => {
+      const scrollTop = window.scrollY;
+      const docHeight = document.documentElement.scrollHeight - window.innerHeight;
+      const scrolled = (scrollTop / docHeight) * 100;
+      progressBar.style.width = scrolled + "%";
+    };
+
+    window.addEventListener("scroll", updateProgress);
+    return () => window.removeEventListener("scroll", updateProgress);
+  }, []);
+
+  return <Component {...pageProps} />;
 }
+
+export default MyApp;
