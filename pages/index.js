b614a281f342f60ff801b754ed4e051c3e45f32b
gngpropertytours-code <gng.propertytours@gmail.com>
2025-10-23 10:22:00 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Plans from "../components/Plans";
 <Plans />
-import Plans from "../components/Plans";
 import FadeInSection from '../components/FadeInSection';
 import Hero from '../components/Hero';
 import HowItWorks from '../components/HowItWorks';
@@ -10,9 +9,10 @@ import HowItWorks from '../components/HowItWorks';
 export default function Home() {
   return (
     <>
-      <FadeInSection><Hero /></FadeInSection>
-      <FadeInSection><HowItWorks /></FadeInSection>
-      <FadeInSection><Plans /></FadeInSection>
+      <Navbar />
+      <Hero />
+      <Plans />
+      <Footer />
     </>
   );
 }
