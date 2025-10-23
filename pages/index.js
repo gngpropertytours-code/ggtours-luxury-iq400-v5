64834acdaf87dbd5474698d687f37618a468d0e0
gngpropertytours-code <gng.propertytours@gmail.com>
2025-10-23 05:26:10 -0400

    Import and add Plans component to Home page

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Hero from '../components/Hero';
 import HowItWorks from '../components/HowItWorks';
+import Plans from '../pages/plans'; // we’re importing the section here
 
 export default function Home() {
   return (
     <>
       <Hero />
       <HowItWorks />
+      <Plans />
     </>
   );
 }
