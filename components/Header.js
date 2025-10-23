901a9f85a6b7c6409aef653ef5b71ae15b72655f
gngpropertytours-code <gng.propertytours@gmail.com>
2025-10-23 11:22:08 -0400

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,17 @@
+import Logo from "./Logo";
+
 export default function Header() {
   return (
-    <header style={{
-      borderBottom: '1px solid',
-      borderImage: 'linear-gradient(90deg,#C8A951,#D4AF37,#EAD27F) 1',
-      padding: '1rem'
-    }}>
-      <h2 style={{ color: '#fff' }}>G & G Property Tours</h2>
+    <header
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: "1rem 2rem",
+      }}
+    >
+      <Logo />
+      {/* Add nav links or buttons here */}
     </header>
   );
 }
